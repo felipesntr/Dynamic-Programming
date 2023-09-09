f9dee9ffdda55de6b94e84b419f5c8254e21be95
Felipe Santos <rfelipe273@gmail.com>
2023-09-09 20:56:21 -0300

    UniquePaths

---
+++ GridTravelerMemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// gridTraveler(1, 1) => 1
+
+int gridTraveler(int m, int n, Dictionary<(int, int), int> memo)
+{
+    if (m == 1 && n == 1) return 1;
+    if (m == 0 || n == 0) return 0;
+
+    if (memo.TryGetValue((m, n), out int value) || memo.TryGetValue((n, m), out value))
+        return value;
+
+    value = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+    memo[(m, n)] = value;
+
+    return value;
+}
