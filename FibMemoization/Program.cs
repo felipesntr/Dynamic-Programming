6059ebef43cdbfa0156b7e6ae7e539bb49e944a4
Felipe Santos <rfelipe273@gmail.com>
2023-09-09 20:24:46 -0300

    Fib memoization

---
+++ FibMemoization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+// Console.WriteLine($"{Fib(50)}");
+
+
+int Fib(int n)
+{
+    if (n <= 2) return 1;
+    return Fib(n - 1) + Fib(n - 2);
+}
+
+// Memoization
+
+BigInteger FibMemo(BigInteger n, Dictionary<BigInteger, BigInteger> memo)
+{
+    if (memo.ContainsKey(n))
+        return memo[n];
+
+    if (n <= 2) return 1;
+    memo.Add(n, FibMemo(n - 1, memo) + FibMemo(n - 2, memo));
+    return memo[n];
+}
+
+Console.WriteLine($"{FibMemo(500, new Dictionary<BigInteger, BigInteger>())}");
