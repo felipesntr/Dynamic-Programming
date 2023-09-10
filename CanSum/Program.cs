f8e8f53c97dad491b138eb71b70a305a135c6772
Felipe Santos <rfelipe273@gmail.com>
2023-09-10 11:20:54 -0300

    CanSum

---
+++ CanSum/Program.cs
@@ -0,0 +1,20 @@
+﻿bool canSum(int targetSum, int[] arr, Dictionary<int, bool> memo)
+{
+    if (memo.TryGetValue(targetSum, out bool value))
+        return value;
+    if (targetSum == 0) return true;
+    if (targetSum < 0) return false;
+    for (int i = 0; i < arr.Count(); i++)
+    {
+        var remainder = targetSum - arr[i];
+        if (canSum(remainder, arr, memo) == true)
+        {
+            memo[targetSum] = true;
+            return true;
+        }
+    }
+    memo[targetSum] = false;
+    return false;
+}
+
+Console.WriteLine(canSum(30000, new int[] { 5, 3, 7 }, new Dictionary<int, bool>())); // true
